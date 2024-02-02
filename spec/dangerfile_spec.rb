c4579ed33a68d852a8d8d177989df6aac27c5978
Andrei Subbota <subbota@gmail.com>
2024-02-02 18:17:08 +0100

    Update dangerfile_spec.rb

--- spec/dangerfile_spec.rb
+++ spec/dangerfile_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe "Dangerfile" do
   it "has no issues" do
     dangerfile_path = Pathname.new("./Dangerfile").realpath
-    stdout_and_stderr, status = Open3.capture2e("danger dry_run --verbose --dangerfile=#{dangerfile_path.to_s}")
+    stdout_and_stderr, status = Open3.capture2e("unset GITHUB_ACTION && danger dry_run --verbose --dangerfile=#{dangerfile_path.to_s}")
 
     expect(status).to be_success, stdout_and_stderr
   end
